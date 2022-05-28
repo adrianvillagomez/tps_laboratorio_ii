c379ef2392e2cd3ea365c16e3acdcbfdf3e6107d
adrianvillagomez <adrian132892@gmail.com>
2022-05-28 10:25:59 -0300

    agregando parcial 3

---
+++ TP3/TP-3/TP-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
